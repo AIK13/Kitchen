59401fa9c63d2d2c791e82ce8fe890167ca7ba4c
AIK13 <aik33308@uga.edu>
2018-02-11 05:15:37 -0500

    Stovetop changes color

---
+++ Kitchen/Assets/Scripts/HeatOnOff.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeatOnOff : MonoBehaviour 
+{
+
+	private int numTimesClicked;
+	public bool heatOn;
+	public Renderer rend;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		numTimesClicked = 0;
+		heatOn = false;
+		rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	
+	void OnMouseDown()
+	{
+		numTimesClicked += 1;
+		
+		if ((numTimesClicked % 2) == 0)
+		{
+			heatOn = false;
+			rend.material.color = Color.white;
+			// it's not just Renderer.material... because Renderer requires the game Object, and
+			// for some reason, it can't just access the current game object... Oh well.
+		}
+		else
+		{
+			heatOn = true;
+			rend.material.color = Color.red;
+		}
+	}
+}
